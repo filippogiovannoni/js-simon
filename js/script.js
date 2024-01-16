42fca8ceae63990473c0f536f70bc728fcafcdae
filippogiovannoni <filippogiovannonii@gmail.com>
2024-01-16 11:52:07 +0100

    Initial commit add set and array for gen numbers

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// Seleziono il wrapper dove inserire il set di numeri generati
+const numbWrapper = document.querySelector('.numbers')
+
+// Creo un set di appoggio dove verranno inseriti i numeri generati
+const numbListSet = new Set();
+
+// Creo un array di appoggio per i numeri scritti dall'utente
+const numbToWriteList = [];
+
